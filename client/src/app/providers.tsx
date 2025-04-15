7adfb99f648deb3a238ef40a3c84beae51df45a0
vicky0718 <vicky187kvs@gmail.com>
2025-04-15 12:07:41 +0530

    full application

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import StoreProvider from "@/state/redux";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import Auth from "./(auth)/authProvider";
 
-const Providers = ({children} : {children: React.ReactNode}) =>  {
-    return (
-        <StoreProvider>
-            {children}
-        </StoreProvider>
-    )
-}
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
+};
 
 export default Providers;
