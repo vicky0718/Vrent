aacc7d9190afec299a36dff0929dc92a11606ef9
vicky0718 <vicky187kvs@gmail.com>
2025-04-29 15:35:37 +0530

    next.config.js changes

--- client/next.config.ts
+++ client/next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: "/**"
       },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+        port: "",
+        pathname: "./**"
+      }
     ]
   }
 };
